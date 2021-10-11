82a333364e018896b50d6da90858d8f760e23482
earthcorp <narendra.s@earthcorp.in>
2021-10-11 09:16:01 +0530

    Update Earthcorp Project

--- components/Header.js
+++ components/Header.js
@@ -187,20 +187,7 @@ function Header() {
                     className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
                     onClick={() => setIsMenuOpen(true)}
                 >
-                    <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
-                    <path
-                        fill="currentColor"
-                        d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
-                    />
-                    <path
-                        fill="currentColor"
-                        d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
-                    />
-                    <path
-                        fill="currentColor"
-                        d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
-                    />
-                    </svg>
+                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16"/></svg>
                 </button>
                 {isMenuOpen && (
                     <div className="absolute top-0 left-0 w-full">
